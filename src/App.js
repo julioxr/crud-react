56145ab481454af2ee155c43b82e4c5a45f50a94
julio <julioroja987@gmail.com>
2022-03-30 19:12:00 -0300

    se creo funcion eliminar tarea

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [tarea, setTarea] = useState();
+    const [tareas, setTareas] = useState([]);
+    const [modoEdidicion, setModoEdicion] = useState(false);
+
+    const agregarTarea = (e) => {
+        e.preventDefault();
+        if (!tarea.trim()) {
+            console.log("Nombre vacio");
+            return;
+        }
+
+        setTareas([...tareas, { id: Date.now(), nombreTarea: tarea }]);
+
+        setTarea("");
+    };
+
+    const eliminarTarea = (id) => {
+        const arrayFiltrado = tareas.filter((item) => item.id !== id);
+        setTareas(arrayFiltrado);
+    };
+
+    return (
+        <div className="container mt-5">
+            <h1 className="text-center">CRUD React</h1>
+            <hr />
+            <div className="row">
+                <div className="col-8">
+                    <h4 className="text-center"> Lista de tareas</h4>
+                    <ul className="list-group">
+                        {tareas.map((item) => (
+                            <li className="list-group-item" key={item.id}>
+                                <span className="lead">{item.nombreTarea}</span>
+                                <button
+                                    className="btn btn-danger btn-sm float-end mx-2"
+                                    onClick={() => eliminarTarea(item.id)}
+                                >
+                                    Eliminar
+                                </button>
+                                <button
+                                    className="btn btn-warning btn-sm float-end"
+                                    onClick={() => eliminarTarea(item.id)}
+                                >
+                                    Editar
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="col-4">
+                    <h4 className="text-center">Formulario</h4>
+                    <form onSubmit={agregarTarea}>
+                        <input
+                            type="text"
+                            className="form-control mb-2"
+                            placeholder="Ingrese Tarea"
+                            onChange={(e) => setTarea(e.target.value)}
+                            value={tarea}
+                        />
+                        <button
+                            className="btn btn-dark btn-block"
+                            type="submit"
+                        >
+                            Agregar
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
