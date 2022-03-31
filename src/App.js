3895a84ae93abd37f1b04e5cd6f626945ecc75e1
julio <julioroja987@gmail.com>
2022-03-30 21:33:11 -0300

    Se agrego opcion de editar tarea

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
     const [tarea, setTarea] = useState();
     const [tareas, setTareas] = useState([]);
     const [modoEdidicion, setModoEdicion] = useState(false);
+    const [id, setId] = useState("");
 
     const agregarTarea = (e) => {
         e.preventDefault();
@@ -22,6 +23,30 @@ function App() {
         setTareas(arrayFiltrado);
     };
 
+    const editar = (item) => {
+        console.log(item);
+        setModoEdicion(true);
+        setTarea(item.nombreTarea);
+        setId(item.id);
+    };
+
+    const editarTarea = (e) => {
+        e.preventDefault();
+        if (!tarea.trim()) {
+            console.log("Vacio");
+            return;
+        }
+
+        const arrayEditado = tareas.map((item) =>
+            item.id == id ? { id, nombreTarea: tarea } : item
+        );
+
+        setTareas(arrayEditado);
+        setModoEdicion(false);
+        setTarea("");
+        setId("");
+    };
+
     return (
         <div className="container mt-5">
             <h1 className="text-center">CRUD React</h1>
@@ -30,28 +55,36 @@ function App() {
                 <div className="col-8">
                     <h4 className="text-center"> Lista de tareas</h4>
                     <ul className="list-group">
-                        {tareas.map((item) => (
-                            <li className="list-group-item" key={item.id}>
-                                <span className="lead">{item.nombreTarea}</span>
-                                <button
-                                    className="btn btn-danger btn-sm float-end mx-2"
-                                    onClick={() => eliminarTarea(item.id)}
-                                >
-                                    Eliminar
-                                </button>
-                                <button
-                                    className="btn btn-warning btn-sm float-end"
-                                    onClick={() => eliminarTarea(item.id)}
-                                >
-                                    Editar
-                                </button>
-                            </li>
-                        ))}
+                        {tareas.length === 0 ? (
+                            <li className="list-group-item">No hay tareas</li>
+                        ) : (
+                            tareas.map((item) => (
+                                <li className="list-group-item" key={item.id}>
+                                    <span className="lead">
+                                        {item.nombreTarea}
+                                    </span>
+                                    <button
+                                        className="btn btn-danger btn-sm float-end mx-2"
+                                        onClick={() => eliminarTarea(item.id)}
+                                    >
+                                        Eliminar
+                                    </button>
+                                    <button
+                                        className="btn btn-warning btn-sm float-end"
+                                        onClick={() => editar(item)}
+                                    >
+                                        Editar
+                                    </button>
+                                </li>
+                            ))
+                        )}
                     </ul>
                 </div>
                 <div className="col-4">
-                    <h4 className="text-center">Formulario</h4>
-                    <form onSubmit={agregarTarea}>
+                    <h4 className="text-center">
+                        {modoEdidicion ? "Editar Tarea" : "Nueva Tarea"}
+                    </h4>
+                    <form onSubmit={modoEdidicion ? editarTarea : agregarTarea}>
                         <input
                             type="text"
                             className="form-control mb-2"
@@ -59,12 +92,22 @@ function App() {
                             onChange={(e) => setTarea(e.target.value)}
                             value={tarea}
                         />
-                        <button
-                            className="btn btn-dark btn-block"
-                            type="submit"
-                        >
-                            Agregar
-                        </button>
+
+                        {modoEdidicion ? (
+                            <button
+                                className="btn btn-warning btn-block"
+                                type="submit"
+                            >
+                                Editar
+                            </button>
+                        ) : (
+                            <button
+                                className="btn btn-dark btn-block"
+                                type="submit"
+                            >
+                                Agregar
+                            </button>
+                        )}
                     </form>
                 </div>
             </div>
